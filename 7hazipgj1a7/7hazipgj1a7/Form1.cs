524704272573ed61eab4ae5957ce01cc751bf328
Rebeka Jákob <rebeka.jakob@stud.uni-corvinus.hu>
2019-12-01 22:02:10 +0100

    Initial commit

---
+++ 7hazipgj1a7/7hazipgj1a7/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7hazipgj1a7
+{
+    public partial class Form1 : Form
+    {
+        BindingList<User> users = new BindingList<User>();
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = Resource1.FullName;
+            //label2.Text = Resource1.FirstName; 
+            button1.Text = Resource1.Add;
+            button2.Text = Resource1.Save;
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                FullName = textBox1.Text,
+
+            };
+            users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.Filter = "Comma Seperated Values (*.csv)|*.csv";
+            sfd.DefaultExt = "csv";
+            sfd.AddExtension = true;
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                foreach (var s in users)
+                {
+
+                    sw.Write(s.ID);
+                    sw.Write(";");
+                    sw.Write(s.FullName);
+                    sw.WriteLine();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            users.Remove((User)listBox1.SelectedItem);
+        }
+    }
+}
